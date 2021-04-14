15846089b57b1b82ba68ac27d3f53b0c6ffb442c
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2021-04-14 08:40:09 -0700

    initial commit

---
+++ export.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2019 Cryptogogue, Inc. All Rights Reserved.
+
+module.exports = {
+    vol:                        require ( './vol.js' ),
+};
---
+++ vol.js
@@ -0,0 +1,74 @@
+// Copyright (c) 2019 Cryptogogue, Inc. All Rights Reserved.
+
+import _                                from 'lodash';
+
+//----------------------------------------------------------------//
+export function decodeAccountRequest ( encoded ) {
+
+    console.log ( 'DECODE ACCOUNT REQUEST' );
+
+    if ( encoded && encoded.length ) {
+        try {
+
+            encoded = encoded.replace ( /(\r\n|\n|\r )/gm, '' );
+            console.log ( 'ENCODED:', encoded );
+
+            const requestJSON = Buffer.from ( encoded, 'base64' ).toString ( 'utf8' );
+            const request = JSON.parse ( requestJSON );
+
+            return request;
+        }
+        catch ( error ) {
+            console.log ( error );
+        }
+    }
+    return false;
+}
+
+//----------------------------------------------------------------//
+export function encodeAccountRequest ( genesis, publicKeyHex ) {
+
+    console.log ( 'ENCODE ACCOUNT REQUEST' );
+
+    const request = {
+        genesis:            this.genesis,
+        key: {
+            type:           'EC_HEX',
+            groupName:      'secp256k1',
+            publicKey:      publicKeyHex,
+        },
+    }
+
+    const requestJSON   = JSON.stringify ( request );
+    const encoded       = Buffer.from ( requestJSON, 'utf8' ).toString ( 'base64' );
+
+    console.log ( 'ENCODED:', encoded );
+
+    return encoded;
+}
+
+//----------------------------------------------------------------//
+export function signTransaction ( key, body, nonce ) {
+
+    const recordBy = new Date ();
+    recordBy.setTime ( recordBy.getTime () + ( 8 * 60 * 60 * 1000 )); // yuck
+
+    body = _.cloneDeep ( body );
+
+    body.maxHeight      = 0; // don't use for now
+    body.recordBy       = recordBy.toISOString ();
+    body.maker.nonce    = nonce;
+
+    const bodyStr = JSON.stringify ( body );
+
+    let envelope = {
+        body: bodyStr,
+        signature: {
+            hashAlgorithm:  'SHA256',
+            digest:         key.hash ( bodyStr ),
+            signature:      key.sign ( bodyStr ),
+        }
+    };
+
+    return envelope;
+}
